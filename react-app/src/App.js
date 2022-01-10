14a16b4848970a004331858c70093697e3587ae7
murshidazher <murshid@different.com.au>
2022-01-10 22:14:19 +0530

    add pr push workflow

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Example pulumi deployment
+          This deployment was done using github PR and Push workflow
         </p>
         <a
           className="App-link"
